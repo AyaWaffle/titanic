ea4a851d9bb3c74795583095101c408675f9f40e
Ayana Murakami <murakami.ayana3@gmail.com>
2022-10-21 02:33:26 +0900

    format

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import streamlit as st
-import pandas as pd
+import datetime
 import logging
-import seaborn as sns
-import data
-import plotly.express as px
 
 import google_auth_httplib2
 import httplib2
+import pandas as pd
+import plotly.express as px
+import seaborn as sns
+import streamlit as st
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import HttpRequest
 
-import datetime
+import data
 
 ALIVE = 1
 DEAD = 0
@@ -23,17 +23,23 @@ SHEET_ID = "1iNr34vpCCUHcguUyDIdH3SUNesS4mUvkgI5oiOHi9ps"
 SHEET_NAME = "titanic1_db"
 
 # Survived,Pclass,Gender,Age,SibSp,Parch,Fare,Embarked
-val_names = ["乗客のクラス(Pclass): 1が高級", "性別(Gender): 0が女性, 1が男性", "年齢(Age)", "乗船していた兄弟、配偶者の人数(SibSp)",
-"乗船していた両親、子供の人数(Parch)", "運賃(Fare)", "乗船した港(Embarked): 0がCherbourg, 1がQueenstown, 2がSouthampton"]
+val_names = [
+    "乗客のクラス(Pclass): 1が高級",
+    "性別(Gender): 0が女性, 1が男性",
+    "年齢(Age)",
+    "乗船していた兄弟、配偶者の人数(SibSp)",
+    "乗船していた両親、子供の人数(Parch)",
+    "運賃(Fare)",
+    "乗船した港(Embarked): 0がCherbourg, 1がQueenstown, 2がSouthampton",
+]
 
 st.set_page_config(
     page_title="Titanic Analysis App",
     layout="wide",
     initial_sidebar_state="collapsed",
-    )
+)
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s,%(message)s")
-
 
 
 # gsheetにログを出力
@@ -70,22 +76,26 @@ def add_row_to_gsheet(gsheet_connector, row):
         valueInputOption="USER_ENTERED",
     ).execute()
 
-DATA_SOURCE = './data/titanic.csv'
+
+DATA_SOURCE = "./data/titanic.csv"
+
 
 @st.cache
 def load_full_data():
     data = pd.read_csv(DATA_SOURCE)
     return data
 
-@st.cache 
+
+@st.cache
 def load_num_data():
     data = pd.read_csv(DATA_SOURCE)
-    rows = ['Survived']
+    rows = ["Survived"]
     data = data.drop(rows, axis=1)
     return data
 
+
 @st.cache
-def load_ML_data(feature1, feature2, train_num = 600):
+def load_ML_data(feature1, feature2, train_num=600):
     df = load_full_data()
     X = df[[feature1, feature2]]
     y = df.Survived  # yはSurvivedの列の値
@@ -100,50 +110,50 @@ def load_ML_data(feature1, feature2, train_num = 600):
 
 def main():
     # If username is already initialized, don't do anything
-    if 'username' not in st.session_state or st.session_state.username == 'default':
-        st.session_state.username = 'default'
+    if "username" not in st.session_state or st.session_state.username == "default":
+        st.session_state.username = "default"
         input_name()
         st.stop()
-    if 'username' not in st.session_state:
-        st.session_state.username = 'test'
-            
-    if 'page' not in st.session_state:
-        st.session_state.page = 'input_name' # usernameつける時こっち
+    if "username" not in st.session_state:
+        st.session_state.username = "test"
 
+    if "page" not in st.session_state:
+        st.session_state.page = "input_name"  # usernameつける時こっち
 
     # --- page選択ラジオボタン
-    st.sidebar.markdown('## ページを選択')
-    page = st.sidebar.radio('', ('データ表示', 'グラフを表示'))
-    if page == 'データ表示':
-        st.session_state.page = 'deal_data'
-        logging.info(',%s,ページ選択,%s', st.session_state.username, page)
-    elif page == 'グラフを表示':
-        st.session_state.page = 'vis'
+    st.sidebar.markdown("## ページを選択")
+    page = st.sidebar.radio("", ("データ表示", "グラフを表示"))
+    if page == "データ表示":
+        st.session_state.page = "deal_data"
+        logging.info(",%s,ページ選択,%s", st.session_state.username, page)
+    elif page == "グラフを表示":
+        st.session_state.page = "vis"
 
     # --- page振り分け
-    if st.session_state.page == 'input_name':
+    if st.session_state.page == "input_name":
         input_name()
-    elif st.session_state.page == 'deal_data':
+    elif st.session_state.page == "deal_data":
         deal_data()
-    elif st.session_state.page == 'vis':
-        vis()      
+    elif st.session_state.page == "vis":
+        vis()
+
 
 # ---------------- usernameの登録 ----------------------------------
 def input_name():
     # Input username
     with st.form("my_form"):
         st.text("メールアドレス b〇〇〇〇〇〇@seig-boys.jp に含まれている番号(学籍番号)を入力")
-        inputname = st.text_input('学籍番号', placeholder='ここに学籍番号を入力')
+        inputname = st.text_input("学籍番号", placeholder="ここに学籍番号を入力")
         submitted = st.form_submit_button("Go!!")
 
         # nameが不適当なら
-        if inputname == '': 
+        if inputname == "":
             submitted = False  # Submit 取り消し
 
         # Submit buttonn 押された時に
         if submitted:
             st.session_state.username = inputname
-            st.session_state.page = 'deal_data'
+            st.session_state.page = "deal_data"
             st.write("名前: ", inputname)
             st.text("↑ 自分の学籍番号であることを確認したら、もう一度 Go! をクリック")
 
@@ -162,18 +172,20 @@ def input_name():
                 ],
             )
 
+
 # ---------------- 訓練データの加工 ----------------------------------
 def deal_data():
     st.title("データの表示")
     full_df = load_full_data()
 
     # highlight の ON/OFF
-    high_light = st.checkbox('最大値をハイライトする')
+    high_light = st.checkbox("最大値をハイライトする")
     if high_light:
         # dataframeを表示
         st.dataframe(full_df.style.highlight_max(axis=0))
     else:
         st.dataframe(full_df)
+
 
 # ---------------- 可視化 :  各グラフを選択する ----------------------------------
 def vis():
@@ -183,13 +195,10 @@ def vis():
     full_data = load_full_data()
     label = feature_data.columns
 
-    st.sidebar.markdown('## 様々なグラフを試してみよう')
+    st.sidebar.markdown("## 様々なグラフを試してみよう")
 
     # sidebar でグラフを選択
-    graph = st.sidebar.radio(
-        'グラフの種類',
-        ('ヒストグラム', '散布図', '全ての散布図')
-    )
+    graph = st.sidebar.radio("グラフの種類", ("ヒストグラム", "散布図", "全ての散布図"))
 
     # 棒グラフ
     # if graph == "棒グラフ":
@@ -227,61 +236,81 @@ def vis():
 
     # ヒストグラム
     if graph == "ヒストグラム":
-        logging.info(',%s,データ可視化,%s', st.session_state.username, graph)
-        st.markdown('## データの分布を見てみよう')
-        st.markdown('赤は生存者の分布、青は死亡者の分布を示す。')
-
+        logging.info(",%s,データ可視化,%s", st.session_state.username, graph)
+        st.markdown("## データの分布を見てみよう")
+        st.markdown("赤は生存者の分布、青は死亡者の分布を示す。")
 
         with st.form("ヒストグラム"):
-            hist_val = st.selectbox('変数を選択',val_names)
+            hist_val = st.selectbox("変数を選択", val_names)
             hist_val = data.get_val(hist_val)
-            logging.info(',%s,ヒストグラム,%s', st.session_state.username, hist_val)
+            logging.info(",%s,ヒストグラム,%s", st.session_state.username, hist_val)
 
             # Submitボタン
-            plot_button = st.form_submit_button('グラフ表示')
-            
+            plot_button = st.form_submit_button("グラフ表示")
+
             if plot_button:
                 if hist_val == "Fare":
-                    g = px.histogram(full_data, x=hist_val, nbins=20, color='Survived', barmode='group')
+                    g = px.histogram(
+                        full_data,
+                        x=hist_val,
+                        nbins=20,
+                        color="Survived",
+                        barmode="group",
+                    )
                     st.plotly_chart(g)
                 elif hist_val == "Age":
-                    g = px.histogram(full_data, x=hist_val, nbins=10, color='Survived', barmode='group')
+                    g = px.histogram(
+                        full_data,
+                        x=hist_val,
+                        nbins=10,
+                        color="Survived",
+                        barmode="group",
+                    )
                     st.plotly_chart(g)
                 else:
-                    g = sns.catplot(x=hist_val, y='Survived', data=full_data, kind='bar', ci=None)
-                    g = px.histogram(full_data, x=hist_val, color='Survived', barmode='group')
+                    g = sns.catplot(
+                        x=hist_val, y="Survived", data=full_data, kind="bar", ci=None
+                    )
+                    g = px.histogram(
+                        full_data, x=hist_val, color="Survived", barmode="group"
+                    )
                     st.plotly_chart(g)
 
         # コードの表示
-        code = st.sidebar.checkbox('コードを表示')
+        code = st.sidebar.checkbox("コードを表示")
         if code:
-            code_txt = "px.histogram(full_data, x=" + hist_val + ", nbins=20, color='Survived', barmode='group')"
-            st.sidebar.markdown('---')
+            code_txt = (
+                "px.histogram(full_data, x="
+                + hist_val
+                + ", nbins=20, color='Survived', barmode='group')"
+            )
+            st.sidebar.markdown("---")
             st.sidebar.write(code_txt)
-            st.sidebar.markdown('---')
-    
+            st.sidebar.markdown("---")
+
     # 散布図
-    elif graph == '散布図':
-        logging.info(',%s,データ可視化,%s', st.session_state.username, graph)
-        st.markdown('## 散布図 で 分布 を調べる')
+    elif graph == "散布図":
+        logging.info(",%s,データ可視化,%s", st.session_state.username, graph)
+        st.markdown("## 散布図 で 分布 を調べる")
         with st.form("散布図"):
             # (Todo) バージョンアップ: 今betaじゃないはず
             left, right = st.beta_columns(2)
 
-            with left: # 変数選択 
-                x_label = st.selectbox('横軸を選択',val_names)
+            with left:  # 変数選択
+                x_label = st.selectbox("横軸を選択", val_names)
                 x_label = data.get_val(x_label)
 
-
             with right:
-                y_label = st.selectbox('縦軸を選択',val_names)
+                y_label = st.selectbox("縦軸を選択", val_names)
                 y_label = data.get_val(y_label)
-            
-            logging.info(',%s,散布図,%s', st.session_state.username, x_label+'_'+ y_label)
-        
+
+            logging.info(
+                ",%s,散布図,%s", st.session_state.username, x_label + "_" + y_label
+            )
+
             # Submitボタン
-            plot_button = st.form_submit_button('グラフ表示')
-            tmp = full_data.copy() # ただ=で複製すると参照渡し → full_dataも値が書き変わる
+            plot_button = st.form_submit_button("グラフ表示")
+            tmp = full_data.copy()  # ただ=で複製すると参照渡し → full_dataも値が書き変わる
             tmp.Survived.replace(DEAD, "dead", inplace=True)
             tmp.Survived.replace(ALIVE, "alive", inplace=True)
 
@@ -293,34 +322,41 @@ def vis():
                 # st.pyplot(g)
 
         # コードの表示
-        code = st.sidebar.checkbox('コードを表示')
+        code = st.sidebar.checkbox("コードを表示")
         if code:
-            code_txt = "g = sns.catplot(x='" +  x_label + "', y='" + y_label + "', data=full_data, kind = 'swarm')"
-            st.sidebar.markdown('---')
+            code_txt = (
+                "g = sns.catplot(x='"
+                + x_label
+                + "', y='"
+                + y_label
+                + "', data=full_data, kind = 'swarm')"
+            )
+            st.sidebar.markdown("---")
             st.sidebar.write(code_txt)
-            st.sidebar.markdown('---')
-    
-    # 散布図行列
-    if graph == '全ての散布図':
-        logging.info(',%s,データ可視化,%s', st.session_state.username, graph)
+            st.sidebar.markdown("---")
 
-        st.markdown('## 全ての変数 を 散布図 に表示する')
-        st.markdown('このグラフの見方は、ページ一番下の「グラフの見方」ボタン参照')
+    # 散布図行列
+    if graph == "全ての散布図":
+        logging.info(",%s,データ可視化,%s", st.session_state.username, graph)
+
+        st.markdown("## 全ての変数 を 散布図 に表示する")
+        st.markdown("このグラフの見方は、ページ一番下の「グラフの見方」ボタン参照")
 
         st.image(data.my_pairplot())
-        
+
         # 散布図行列の見方を表示
-        reference_btn = st.button('グラフの見方')
+        reference_btn = st.button("グラフの見方")
         if reference_btn:
             st.image(data.how_to_check())
 
         # コードの表示
-        code = st.sidebar.checkbox('コードを表示')
+        code = st.sidebar.checkbox("コードを表示")
         if code:
             code_txt = "g = sns.pairplot(full_data, hue='Survived', palette='seismic')"
-            st.sidebar.markdown('---')
+            st.sidebar.markdown("---")
             st.sidebar.write(code_txt)
-            st.sidebar.markdown('---')
+            st.sidebar.markdown("---")
+
 
 gsheet_connector = connect_to_gsheet()
 main()
